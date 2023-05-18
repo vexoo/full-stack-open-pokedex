b20ff727d49e6985a81320c1400142339ee6188b
vexoo <jntv90@gmail.com>
2023-05-18 12:17:24 +0300

    11.9 should work

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,7 +1,19 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+/* global cy */
+
+describe('Pokedex', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+
+  it('can navitage to a page of a pokemon', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
